91493e3be67c88d80a9ecb440faff0471005f9ea
IgnacioRG <IgnacioRG@ciencias.unam.mx>
2023-11-09 13:58:49 -0600

    Contecta2 VisualFix AllLvL
    Terminados los niveles 11 y 12, además de las líneas distractoras de todos los niveles. Se empezó la revisión de errores en el inspector.

---
+++ Conecta2/Scripts/Aggrandizer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+//Script para aumentar el tamaño de los BoxCollider de las lineas distractoras.
+public class Aggrandizer : MonoBehaviour
+{
+    public GameObject distractoras_lines;
+
+    private void Awake()
+    {
+        AgrandizaElJocco(20, 10);
+    }
+
+    /**
+     * AgrandizaElJocco Recorre los hijos de los objetos de juego que contienten
+     * las lineas distractoras de los niveles.
+     */
+    private void AgrandizaElJocco(int wth, int hgh)
+    {
+        for (int i = 0; i < distractoras_lines.transform.childCount; i++)
+        {
+            for (int j = 0; j < distractoras_lines.transform.GetChild(i).childCount; j++)
+            {
+                GameObject linea = distractoras_lines.transform.GetChild(i).GetChild(j).gameObject;
+                float wline = linea.GetComponent<RectTransform>().rect.width;
+                linea.GetComponent<RectTransform>().sizeDelta = new Vector2(wline + wth, hgh);
+                linea.GetComponent<BoxCollider>().size = new Vector2(wline + wth, hgh);
+            }
+        }
+    }
+}
+
